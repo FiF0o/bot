50f1be6bea4b4e9b14fd29b23573d7712c10559d
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-07-12 12:09:05 +0100

    implement sentiment and send response back to user

--- app.js
+++ app.js
@@ -6,22 +6,50 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 var sentiment = require('sentiment');
-const request = require('request');
+var request = require('request');
+require('dotenv').config();
+/**
+ * -- *** Slack App configuration *** --
+ *
+ * After creating your App,
+ * * store your Verification Token
+ * * you can also store your Client ID in case you need to implement the OAuth when distributing the app
+ *
+ * Add the required features
+ * * 1-bots
+ * * 2-OAuth Tokens & permissions
+ * * * Add the relevant permission scopes to your app: bot, channels:history
+ * * * Store your OAuth Access Token for later
+ * * 3-Event subscriptions
+ * * * Enable Events with your Request URL (with ngrok until you deploy it) used for Slack API to post requests, must do the challenge verification
+ * * * Subscribe your bot to events: message.channels
+ *
+ * Add the bot to the desired channel (not private)
+ */
 
 
-function postEmotion(emotion, ev) {
+var BOT_NAME = 'Sentiment Bot';
+
+
+/**
+ *
+ * @param emotion
+ * @param ev
+ */
+function postEmotion(emotion, userPayload) {
   console.log('emotion in postEmotion', emotion)
-  console.log('ev in postEmotion', ev)
-    var message = 'feeling ' + emotion;
+    //TODO Replace later with function output processing data
+    var message = 'feeling ' + emotion.score;
     var options = {
         method: 'POST',
+        // method, Web API endpoint to use for sending messages
         uri: 'https://slack.com/api/chat.postMessage',
         form: {
             token: process.env.SLACK_OAUTH_TOKEN,
-            channel: ev.channel,
+            channel: userPayload.channel,
             text: message,
             as_user: false,
-            username: 'Sentiment Bot'
+            username: BOT_NAME
         }
     };
     // Use Request module to POST
@@ -55,10 +83,38 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/bot', function(req, res, next) {
-  var q = req.body;
-  console.log('Q\n', q)
+// var ts = require('tinyspeck');
+// var slack = ts.instance({ token: process.env.SLACK_OAUTH_TOKEN });
+/*var instance = slack.instance({
+    unfurl_links: true,
+    channel: 'C1QD223DS1',
+    token: 'xoxb-12345678900-ABCD1234567890'
+})*/
+// console.log('slack', slack)
 
+/*slack.on('message.channels', function(payload){
+    console.log('payload', payload);
+    const message = {
+        text: "I am a test message http://slack.com",
+        attachments: [{
+            text: "And here's an attachment!"
+        }]
+    };
+
+    slack.send('chat.postMessage', message)
+        .then(function(res) {
+            console.log('res', res)
+    })
+})*/
+
+// slack.listen('4000')
+
+
+
+app.post('/bot', function(req, res, next) {
+
+
+  var q = req.body;
     // request is coming from slack?
     if (q.token !== process.env.SLACK_VERIFICATION_TOKEN) {
         res.sendStatus(400);
@@ -66,20 +122,27 @@ app.post('/bot', function(req, res, next) {
     }
 
     if (req.body.type === 'url_verification') {
-        res.send(q.challenge); // complete bot endpoint handshake for Slack
+        res.status(200).send(q.challenge) // complete bot endpoint handshake for Slack
     }
 
     // 2. Events - get the message text
     else if (q.type === 'event_callback') {
-        if(!q.event.text) return;
-        console.log('q.event\n', q.event);
-        var sent = sentiment(q.event.text) // sentiment analysis
-        console.log('sent:\n', sent)
-        // postEmotion(sent, q.event) // post message
+        var userPayload = q.event;
+        console.log('q.event aka userPayload\n', userPayload);
+        if(!userPayload.text) return; // no message sent
+        var sent = sentiment(userPayload.text) // sentiment analysis
+        //TODO 'Data processing' required here depending on the score sentiment
+        /**
+         *
+         */
+        // post message which is not from the bot to avoid infinite loop
+        if (q.event.username !== BOT_NAME) postEmotion(sent, userPayload)
 
         res.sendStatus(200)
     }
 });
+
+
 
 app.use('/', index);
 app.use('/users', users);
